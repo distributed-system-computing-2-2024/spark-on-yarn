10d4c351df909f278cbb63be709e4a7958584377
0703kyj <kyj0703@konkuk.ac.kr>
2024-06-08 15:38:56 +0900

    chore: src 폴더 볼륨 마운트

--- startHadoopCluster.sh
+++ startHadoopCluster.sh
@@ -23,7 +23,7 @@ done
 # START HADOOP MASTER
 
 HADOOP_MASTER="$HOST_PREFIX"-master
-docker run --name $HADOOP_MASTER -h $HADOOP_MASTER --net=$NETWORK_NAME \
+docker run --name $HADOOP_MASTER -v /Users/jun/Desktop/hadoop/docker-spark-yarn-cluster/src:/src -h $HADOOP_MASTER --net=$NETWORK_NAME \
 		-p  8088:8088  -p 50070:50070 -p 50090:50090 \
 		-p  8080:8080 \
 		-itd "$IMG_NAME"
