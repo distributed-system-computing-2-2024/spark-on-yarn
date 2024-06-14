c20dc263fb9bc35e4192cd7142415411a5a3f521
0703kyj <kyj0703@konkuk.ac.kr>
2024-06-14 16:26:32 +0900

    feat: 카산드라 연결

--- README.md
+++ README.md
@@ -31,6 +31,18 @@ spark-shell --master yarn --deploy-mode client
 spark-submit --master yarn --deploy-mode client --num-executors 2 --executor-memory 4G --executor-cores 4 --class org.apache.spark.examples.SparkPi $SPARK_HOME/examples/jars/spark-examples_2.11-2.4.1.jar
 ```
 ```bash
-spark-submit --master yarn --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.8 src/consume.py
+spark-submit --master yarn --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.8,com.datastax.spark:spark-cassandra-connector_2.11:2.4.1 \
+    --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:/src/log4j.properties -Dfile.encoding=UTF-8" \
+    --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:/src/log4j.properties -Dfile.encoding=UTF-8" \
+    --files "/src/log4j.properties" \
+    src/consume.py
 ```
+create table road
+(
+    road_id          text primary key,
+    congestion_level text,
+    road_name        text,
+    std_date        text,
+    std_hour        text,
+);
 
--- src/consume.py
+++ src/consume.py
@@ -1,56 +1,80 @@
 import findspark
-
 from pyspark.sql import SparkSession
-
-# Cassandra
-import os
-
-os.environ['PYSPARK_SUBMIT_ARGS'] = (
-    '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,'
-    'com.datastax.spark:spark-cassandra-connector_2.11:2.4.1 '
-    '--conf spark.cassandra.connection.host=localhost '
-    'pyspark-shell'
-)
+from pyspark.sql.functions import from_json, col, expr
+from pyspark.sql.types import StructType, StructField, StringType
 
 findspark.init()
 
 if __name__ == "__main__":
-    def handle_rdd(rdd):
-        if not rdd.isEmpty():
-            global cassandraSparkSession
-            dataframe = cassandraSparkSession.createDataFrame(rdd, schema=['word', 'count'])
-            dataframe.show(5)
-            dataframe.write \
-                .format("org.apache.spark.sql.cassandra") \
-                .mode('append') \
-                .options(table="words", keyspace="streaming_test") \
-                .save()
 
-
-    cassandraSparkSession = SparkSession.builder \
-        .appName('SparkCassandraApp') \
-        .config('spark.cassandra.connection.host', 'localhost') \
-        .config('spark.cassandra.connection.port', '9042') \
-        .config('spark.cassandra.output.consistency.level', 'ONE') \
+    # SparkSession 생성
+    spark = SparkSession.builder \
+        .appName('SparkKafkaCassandraApp') \
+        .config("spark.cassandra.connection.host", "cassandra1") \
+        .config("spark.cassandra.connection.port", "9042") \
         .getOrCreate()
 
-    kafkaSparkSession = SparkSession.builder.getOrCreate()
+    # Kafka 메시지 스키마 정의
+    schema = StructType([
+        StructField("stdHour", StringType(), True),
+        StructField("routeNo", StringType(), True),
+        StructField("routeName", StringType(), True),
+        StructField("updownTypeCode", StringType(), True),
+        StructField("vdsId", StringType(), True),
+        StructField("trafficAmout", StringType(), True),
+        StructField("shareRatio", StringType(), True),
+        StructField("conzoneId", StringType(), True),
+        StructField("conzoneName", StringType(), True),
+        StructField("stdDate", StringType(), True),
+        StructField("speed", StringType(), True),
+        StructField("timeAvg", StringType(), True),
+        StructField("grade", StringType(), True),
+    ])
 
-    message = kafkaSparkSession.readStream.format("kafka") \
-        .option("kafka.bootstrap.servers", "localhost:9092") \
+    # Kafka에서 스트리밍 데이터 읽기
+    kafka_df = spark.readStream.format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka:9092") \
         .option("subscribe", "trafficSystem") \
         .option("startingOffsets", "earliest") \
+        .option("failOnDataLoss", "false") \
         .load()
 
-    # lines = message.map(lambda x: x[1])
-    # transform = lines.map(lambda tweet: (tweet, int(len(tweet.split()))))
-    # transform.foreachRDD(handle_rdd)
-    # Write stream - console
-    query = message.selectExpr("CAST(value AS STRING)") \
-        .writeStream \
+    df = kafka_df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+    # JSON 형식의 value를 파싱하여 데이터 프레임으로 변환
+    parsed_df = df.withColumn("Body", from_json(col("value"), schema)).select(col("Body.*"))
+
+    # 결과 출력
+    query = parsed_df.writeStream \
+        .outputMode("append") \
         .format("console") \
         .option("truncate", "false") \
         .start()
 
-    query.awaitTermination()
+    transformed_df = parsed_df.select(
+        col("vdsId").alias("road_id"),
+        col("conzoneName").alias("road_name"),
+        col("stdDate").alias("std_date"),
+        col("stdHour").alias("std_hour"),
+        expr("CASE "
+            "WHEN CAST(speed AS INT) >= 80 THEN 'low' "
+            "WHEN CAST(speed AS INT) BETWEEN 30 AND 79 THEN 'middle' "
+            "ELSE 'high' "
+            "END").alias("congestion_level")
+    )
 
+    def foreach_batch_function(batch_df, batch_id):
+        batch_df.write \
+            .format("org.apache.spark.sql.cassandra") \
+            .options(table="road", keyspace="dsc2024") \
+            .mode("append") \
+            .save()
+
+    # Cassandra에 데이터 저장
+    query2 = transformed_df.writeStream \
+        .foreachBatch(foreach_batch_function) \
+        .option("checkpointLocation", "/tmp/checkpoints") \
+        .start()
+
+    query.awaitTermination()
+    query2.awaitTermination()
